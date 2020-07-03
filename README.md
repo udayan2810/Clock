2ec995e83280c75e6157b20b129b56e0dcde1dde
udayan2810 <64144265+udayan2810@users.noreply.github.com>
2020-07-03 19:00:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Clock made by Udayan2810
---
+++ sketch.js
@@ -0,0 +1,53 @@
+function setup() {
+  createCanvas(400,400);
+  angleMode(DEGREES)
+ 
+}
+
+function draw() {
+  background(0); 
+  translate(200,200);
+  rotate(-90)
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+
+
+strokeWeight(8);
+stroke(255,100,150);
+noFill();
+let end1  = map(sc, 0 , 60 , 0 , 360)
+arc(0,0,300,300,0,end1);
+
+push()
+rotate(end1); 
+stroke(255,100,150);
+line(0,0,100,0);
+pop();
+
+stroke(150,100,255);
+let end2  = map(mn, 0 , 60 , 0 , 360)
+arc(0,0,280,280,0,end2);
+
+push()
+rotate(end2); 
+stroke(150,100,255);
+line(0,0,75,0);
+pop();
+
+stroke(150,255,100);
+let end3  = map(hr % 12 , 0 , 12 , 0 , 360)
+arc(0,0,260,260,0,end3);
+
+push()
+rotate(end3); 
+stroke(150,255,100);
+line(0,0,50,0);
+pop();
+
+
+  //fill (255);
+  //noStroke();
+  //text(hr + ':' + mn + ':' + sc,400,450);
+  drawSprites();
+}
